cbdaba7525c83b715bcf3bca0616988a29dd940e
777Safal <tsafal06@gmail.com>
2023-11-11 15:03:51 +0545

    new code

---
+++ components/PageComponent/BecomePartner/Partner/Partners.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import Shape from './Shape'
+
+function Partners() {
+    const data=[
+        {h1:"Refer Partners",
+        p:"ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+        },
+        {h1:"Franchise Partner",
+        p:"ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+        },
+    ]
+  return (
+    <div className='w-full py-10'>
+        <div className='w-full h-fit relative'>
+            <div className='w-full h-fit mx-auto flex flex-col md:flex-row lg:flex-row xl:flex-row xxl:flex-row'>
+                <div className='w-full md:w-2/4 lg:w-2/4 xl:w-2/4 xxl:w-2/4 md:pl-12 lg:pl-16 xl:pl-20 xxl:pl-28 h-fit px-16 sm:px-20 md:px-0 lg:px-0 xl:px-0 xxl:px-0'>
+                    <h1 className='w-fit border-b-4 pb-2 border-purple text-xl md:text-2xl lg:text-2xl xl:text-3xl xxl:text-3xl my-0'>
+                        Partners
+                    </h1>
+                    <div className='my-6 flex flex-col gap-5'>
+                        {data.map((val,i)=>{
+                            return <div key={i}>
+                                <h1 className='text-lg md:text-xl lg:text-xl xl:text-2xl xxl:text-2xl font-normal my-0'>
+                                    {val.h1}
+                                </h1>
+                                <p className='md:pr-10 lg:max-w-[38ch] xl:max-w-[48ch] xxl:max-w-[55ch] text-sm md:text-base lg:text-base xl:text-lg xxl:text-xl font-light md:leading-6 lg:leading-5 xl:leading-6 xxl:leading-6 text-justify my-2'>
+                                    {val.p}
+                                </p>
+                                <div className=''>
+                                    <button className='w-20 sm:w-28 md:w-32 lg:w-40 xl:w-44 xxl:w-48 h-8 md:h-9 lg:h-9 xl:h-10 xxl:h-12 my-2 md:my-3 lg:my-4 xl:my-5 xxl:my-5 text-sm md:text-base lg:text-base xl:text-base xxl:text-base capitalize text-center border-2 rounded-3xl mx-2 border-purple hover:bg-white hover:scale-110 hover:text-purple bg-purple text-white transition-all duration-400 ease-in-out'>
+                                        join now 
+                                    </button>
+                                    <button className='w-28 sm:w-28 md:w-32 lg:w-40 xl:w-44 xxl:w-48 h-8 md:h-9 lg:h-9 xl:h-10 xxl:h-12 my-2 md:my-3 lg:my-4 xl:my-5 xxl:my-5 text-sm md:text-base lg:text-base xl:text-base xxl:text-base capitalize text-center  border-2 rounded-3xl mx-2 border-purple bg-white text-purple hover:bg-purple hover:scale-110 hover:text-white
+                                    transition-all duration-400 ease-in-out'>
+                                        Explore more
+                                    </button>
+                                </div>
+                            </div>
+                        })}
+                    </div>
+                </div>
+                <div className='w-full md:w-2/4 lg:w-2/4 xl:w-2/4 xxl:w-2/4 relative px-16 sm:px-20 md:px-0 lg:px-0 xl:px-0 xxl:px-0'>
+                    <h1 className='w-fit border-b-4 pb-2 border-purple text-xl md:text-2xl lg:text-2xl xl:text-3xl xxl:text-3xl my-0'>
+                    Already Have A Partner Account ?
+                    </h1>
+                    <button className='w-20 md:w-24 lg:w-28 xl:w-40 xxl:w-40 h-8 md:h-9 lg:h-9 xl:h-10 xxl:h-12 text-sm md:text-base lg:text-base xl:text-base xxl:text-base mt-5 capitalize text-center border-2 rounded-3xl mx-2 border-purple hover:bg-white hover:scale-110 hover:text-purple bg-purple text-white transition-all duration-400 ease-in-out'>
+                    Log in
+                    </button>
+                    <div className='hidden md:block lg:block xl:block xxl:block w-full md:h-[300px] lg:h-[380px] xl:h-[400px] xxl:h-[450px] absolute md:top-32 lg:bottom-10 xxl:bottom-4 right-0'>
+                        <Shape/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Partners
---
+++ components/PageComponent/BecomePartner/Partner/PratnerTop.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function PratnerTop() {
+  return (
+    <div className='bg-[#FACC15] w-full relative h-36 sm:h-52 md:h-56 lg:h-60 xl:h-60 xxl:h-64 overflow-hidden'>
+            <div className='w-10/12 mx-auto mt-6 sm:mt-10 md:mt-14 lg:mt-14 xl:mt-[3% xxl:mt-16'>
+                <h1 className='text-2xl md:text-3xl lg:text-4xl xl:text-4xl xxl:text-5xl capitalize my-0'>
+                    Become A Partner
+                </h1>
+                <p className='text-sm md:text-base lg:text-base xl:text-lg xxl:text-xl'>
+                    Home/Become A Partner
+                </p>
+            </div>
+            <div className='bg-white absolute -left-3 -bottom-16 xxl:-bottom-20 w-[120%] h-20 sm:h-24 md:h-24 lg:h-28 xl:h-28 xxl:h-32 rotate-[4deg]'>
+            </div>
+        </div>
+  )
+}
+
+export default PratnerTop
